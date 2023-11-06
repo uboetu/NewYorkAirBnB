9c24c57dafc7444802e563dc954e8da68b607d56
KjeldBoersma <145853028+KjeldBoersma@users.noreply.github.com>
2023-11-06 15:19:20 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -47,38 +47,32 @@ if selection == "Home":
     st.title("Distribution Analysis")
     
     # Display the code in the Streamlit app
-    st.code("""
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import seaborn as sns
+    fig=       
+        fig, axes = plt.subplots(3, 2, figsize=(14, 10))
+        axes = axes.flatten()
     
-    fig, axes = plt.subplots(3, 2, figsize=(14, 10))
-    axes = axes.flatten()
-    
-    log_transform_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count']
-    numerical_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365']
-    summary_stats = dataset[numerical_cols].describe()
-    
-    for i, col in enumerate(numerical_cols):
-        ax = axes[i]
-        data = dataset[col]
-        if col in log_transform_cols:
-            data = np.log1p(data)
-            ax.set_title(f'Log-transformed Distribution of {col}')
-        else:
-            ax.set_title(f'Distribution of {col}')
-        sns.histplot(data, bins=30, kde=True, ax=ax)
-        ax.set_xlabel(col)
-        ax.set_ylabel('Frequency')
-    
-    plt.tight_layout()
-    plt.show()
-    
-    summary_stats
-    """, language="python")
-    
+        log_transform_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count']
+        numerical_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365']
+        summary_stats = dataset[numerical_cols].describe()
+        
+        for i, col in enumerate(numerical_cols):
+            ax = axes[i]
+            data = dataset[col]
+            if col in log_transform_cols:
+                data = np.log1p(data)
+                ax.set_title(f'Log-transformed Distribution of {col}')
+            else:
+                ax.set_title(f'Distribution of {col}')
+            sns.histplot(data, bins=30, kde=True, ax=ax)
+            ax.set_xlabel(col)
+            ax.set_ylabel('Frequency')
+        
+        plt.tight_layout()
+        plt.show()
+        
+        
     # Display the plot in the Streamlit app
-    st.write("### Distribution of Numerical Columns")
+    st.write("### Distribution of Numerical Columns")     
     st.pyplot(fig)    
     # Display the summary statistics if needed
     st.write(summary_stats)
