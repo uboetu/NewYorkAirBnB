a3cbf6a45d34d319ef6505fc8a17100dc426340c
Kjeld Boersma <kjeld.boersma@hva.nl>
2023-11-06 14:59:44 +0100

    poepies

--- Dashboard.py
+++ Dashboard.py
@@ -37,7 +37,62 @@ if selection == "Home":
             """
     )     
    
-  
+    dataset = pd.read_csv('AB_NYC_2019.csv')
+    dataset.head()
+    dataset.info()
+
+    fig, axes = plt.subplots(3, 2, figsize=(14, 10))
+
+    axes = axes.flatten()
+
+
+
+    # Specify columns to apply log transformation
+
+    log_transform_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count']
+    numerical_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365']
+    summary_stats = dataset[numerical_cols].describe()
+
+
+    for i, col in enumerate(numerical_cols):
+
+        ax = axes[i]
+
+        data = dataset[col]  # Using 'dataset' instead of 'df'
+
+        
+
+        # Apply log transformation if column is in the list
+
+        if col in log_transform_cols:
+
+            # Adding a small constant to handle zero values
+
+            data = np.log1p(data)
+
+            ax.set_title(f'Log-transformed Distribution of {col}')
+
+        else:
+
+            ax.set_title(f'Distribution of {col}')
+
+        
+
+        sns.histplot(data, bins=30, kde=True, ax=ax)
+
+        ax.set_xlabel(col)
+
+        ax.set_ylabel('Frequency')
+
+
+
+    plt.tight_layout()
+
+    plt.show()
+
+
+
+    summary_stats
 
 
 elif selection == "Charging Station Data":
