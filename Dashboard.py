c287c834255d0cab07152afe6f0a52a550f9663c
KjeldBoersma <145853028+KjeldBoersma@users.noreply.github.com>
2023-11-06 15:13:21 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -43,60 +43,45 @@ if selection == "Home":
     
     dataset.info()
 
+    # Create a Streamlit app
+    st.title("Distribution Analysis")
+    
+    # Display the code in the Streamlit app
+    st.code("""
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import seaborn as sns
+    
     fig, axes = plt.subplots(3, 2, figsize=(14, 10))
-
     axes = axes.flatten()
-
-
-
-    # Specify columns to apply log transformation
-
+    
     log_transform_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count']
     numerical_cols = ['price', 'minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365']
     summary_stats = dataset[numerical_cols].describe()
-
-
+    
     for i, col in enumerate(numerical_cols):
-
         ax = axes[i]
-
-        data = dataset[col]  # Using 'dataset' instead of 'df'
-
-        
-
-        # Apply log transformation if column is in the list
-
+        data = dataset[col]
         if col in log_transform_cols:
-
-            # Adding a small constant to handle zero values
-
             data = np.log1p(data)
-
             ax.set_title(f'Log-transformed Distribution of {col}')
-
         else:
-
             ax.set_title(f'Distribution of {col}')
-
-        
-
         sns.histplot(data, bins=30, kde=True, ax=ax)
-
         ax.set_xlabel(col)
-
         ax.set_ylabel('Frequency')
-
-
-
+    
     plt.tight_layout()
-
     plt.show()
-
-    st.pyplot(plt)
-
-
+    
     summary_stats
-
+    """, language="python")
+    
+    # Display the plot in the Streamlit app
+    st.pyplot(fig)
+    
+    # Display the summary statistics if needed
+    st.write(summary_stats)
 
 elif selection == "Data":
     st.title("gg")
