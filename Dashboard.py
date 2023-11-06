77b85924a9884c9fc1346bbb588bbb5afc771904
KjeldBoersma <145853028+KjeldBoersma@users.noreply.github.com>
2023-11-06 14:13:41 +0100

    Create Dashboard.py

---
+++ Dashboard.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import requests
+import folium
+import numpy as np
+import math
+import missingno as msno
+import seaborn as sns
+import altair as alt
+import matplotlib.pyplot as plt
+from folium.plugins import MarkerCluster
+import streamlit as st
+from streamlit_folium import folium_static
+from folium import Element
+from ipywidgets import interact
+import json
+import html
+
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", ["Home", "Hoer", "Slet", "Je mama", "Je oma"])
+
+# Main content
+if selection == "Home":
+    st.title("Hoeren")
+    st.subheader("Hoeren zooi")
+    st.markdown(
+
+    """
+    
+   
+
+    
+            """
+    )     
+   
+  
+
+
+elif selection == "Charging Station Data":
